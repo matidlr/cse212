c54119e90eb94a38babc57d2a259967b9a58e5f5
matidlr <argentinavende.sa@gmail.com>
2026-02-23 13:04:21 -0300

    SimpleQueue

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,14 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        double[] result = new double[length];
+
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = number * (i + 1);
+    }
+
+    return result;
     }
 
     /// <summary>
@@ -29,5 +36,14 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        int splitIndex = data.Count - amount;
+
+    List<int> firstPart = data.GetRange(0, splitIndex);
+    List<int> secondPart = data.GetRange(splitIndex, amount);
+
+    data.Clear();
+
+    data.AddRange(secondPart);
+    data.AddRange(firstPart);
     }
 }
